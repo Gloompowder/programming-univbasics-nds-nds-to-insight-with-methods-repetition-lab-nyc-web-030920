f78200c27243e90ca90ecb8108c7a7dc09709f35
William Lin <willlin1996@gmail.com>
2020-02-29 23:47:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  list_of_directors(source)
+  list_of_directors(source)[]
 end
 
 
