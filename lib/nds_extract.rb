80bf23ef75e4fac8c82793f79c119ad4a99784fc
William Lin <willlin1996@gmail.com>
2020-02-29 21:13:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,10 +26,10 @@ end
 
 def list_of_directors(source)
   # Write this implementation
-  # name_index = 0
-  # while name_index < source.length do
+  name_index = 0
+  while name_index < source.length do
     
-  #   name_index += 1
+    name_index += 1
   end
 end
 
