dbf9697e1bb94d736e4abd2ff2923bfa594a14fc
William Lin <willlin1996@gmail.com>
2020-03-01 00:26:31 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,7 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
   total = 0
-  directors_totals(source)[list_of_directors(source)].sum
+  (directors_totals(source)[list_of_directors(source)]).sum
   # total
 end
 
